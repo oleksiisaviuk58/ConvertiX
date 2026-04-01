fd4370038f1b176c6bab55806acfc5497d7f778e
Oleksii Saviuk <oleksiisaviuk58@gmail.com>
2026-04-01 22:30:25 +0300

    feat: add result output and input validation for currency conversion

--- ConvertiX/Controllers/ConverterController.cs
+++ ConvertiX/Controllers/ConverterController.cs
@@ -6,7 +6,16 @@ public class ConverterController : Controller
 {
     [HttpGet("/")]
     public ViewResult Index() => View();
-    
-    [HttpPost("/convert")]
-    public IActionResult Convert() => Ok("CONVERT!");
+
+    [HttpPost("/")]
+    public ViewResult Index(string amount, string from, string to)
+    {
+        if (!string.IsNullOrEmpty(amount))
+        {
+            if (decimal.TryParse(amount, out decimal result))
+                ViewBag.Result = result * 2;
+        }
+        
+        return View();
+    }
 }
